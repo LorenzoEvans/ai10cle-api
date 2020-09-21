6694430b163e3f236344b5aaaec43cbc82f6df77
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-09-21 12:41:22 -0700

    Adding more fleshed out handlers for api requests

--- src/handlers.rs
+++ src/handlers.rs
@@ -17,15 +17,24 @@ pub struct InputUser {
     pub email: String,
 }
 pub async fn get_users(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    format!("hello from get users")
+    Ok(web::block(move || get_all_users(db))
+        .await
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?)
 }
 
-pub async fn get_user_by_id(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    format!("hello from get users by id")
+pub async fn get_user_by_id(db: web::Data<Pool>, user_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || db_get_user_by_id(db, user_id.into_inner()))
+        .await
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?)
 }
 
-pub async fn add_user(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    format!("hello from add user")
+pub async fn add_user(db: web::Data<Pool>, item: web::Json<InputUser>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || add_single_user(db))
+        .await
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?)
 }
 
 pub async fn delete_user(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
@@ -35,6 +44,15 @@ pub async fn delete_user(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
 pub async fn get_articles(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from get articles")
 }
+fn get_all_users(pool: web::Data<Pool>) -> Result<Vec<User>, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    let items = users.load::<User>(&conn)?;
+    Ok(items)
+}
+
+fn db_get_user_by_id(){}
+fn add_single_user(){}
+fn delete_single_user(){}
 
 // pub async fn save_articles
 
