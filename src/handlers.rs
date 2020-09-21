5bd625ceadd6b18711d7a412c9f0ca8597805678
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-09-20 17:02:16 -0700

    fleshing out handler functions

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,22 +1,38 @@
-use actix_web::Responder;
+use actix_web::{web, Error, HttpResponse};
+use super::models::{NewUser, User};
+use super::schema::users::dsl::*;
+use super::Pool;
+use crate::diesel::QueryDsl;
+use crate::diesel::RunQueryDsl;
+use diesel::dsl::{delete, insert_into};
+use serde::{Deserialize, Serialize};
+use std::vec::Vec;
 
-pub async fn get_users() -> impl Responder {
+
+#[derive(Debug, Serialize, Deserialize)]
+
+pub struct InputUser {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
+pub async fn get_users(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from get users")
 }
 
-pub async fn get_user_by_id() -> impl Responder {
+pub async fn get_user_by_id(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from get users by id")
 }
 
-pub async fn add_user() -> impl Responder {
+pub async fn add_user(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from add user")
 }
 
-pub async fn delete_user() -> impl Responder {
+pub async fn delete_user(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from delete user")
 }
 
-pub async fn get_articles() -> impl Responder {
+pub async fn get_articles(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     format!("hello from get articles")
 }
 
