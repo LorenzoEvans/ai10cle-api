809bd11737e748ca2588f1b8965676c34525c211
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-09-21 16:33:45 -0700

    api w/ users done, working on login/logout next

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{dev::ServiceRequest, Error, web, App, HttpServer};
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 #[macro_use]
@@ -14,8 +17,10 @@ mod handlers; // this module will contain our handlers
 mod errors;
 mod models; // Models for our data base
 mod schema; // Models for state (and then database)
+mod auth;
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 #[actix_rt::main]
+
 async fn main() -> std::io::Result<()> {
     // This returns a Result type
     dotenv::dotenv().ok(); // Activate dotenv as early in app as possible
@@ -30,7 +35,9 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to create pool.");
 
     HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(validate);
         App::new()
+            .wrap(auth)
             .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
@@ -39,4 +46,22 @@ async fn main() -> std::io::Result<()> {
     }).bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+async fn validate(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.get_ref().clone())
+        .unwrap_or_else(Default::default);
+    match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res == true {
+                Ok(req)
+            }
+            else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into())
+    }
 }
