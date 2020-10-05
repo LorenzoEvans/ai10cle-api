14281e92d303edc72d03cea35d656e1c3da21cd6
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-10-05 12:44:03 -0700

    alright lets see

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,10 @@ async fn main() -> std::io::Result<()> {
 
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
-    let ip: &str = "0.0.0.0";
-    let port = 5000;
+    // let ip: &str = "0.0.0.0";
+    // let port = 5000;
     let port_val = std::env::var("PORT").unwrap_or_else(|_| "0.0.0.0:5000".to_string());
+    let host_val = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0:5000".to_string());
     let pool: Pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
@@ -70,7 +71,7 @@ async fn main() -> std::io::Result<()> {
                 .route("/users/{id}", web::get().to(user_handlers::get_user_by_id))
                 .route("/users", web::post().to(user_handlers::add_user))
                 .route("/users/{id}", web::delete().to(user_handlers::delete_user)))   
-    }).bind(port_val)?  // ? Bubbles up errors from the associated function.                       // Bind attaches a socket address to the application.
+    }).bind((host_val, port_val))?  // ? Bubbles up errors from the associated function.                       // Bind attaches a socket address to the application.
     .run() // Returns an instance of Server type.
     .await
 }
